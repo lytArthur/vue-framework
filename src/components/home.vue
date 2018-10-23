3897f0243c945401e17aed18b546d0817536e725
wanggang <wanggang@wanggangdeMacBook-Pro.local>
2018-10-23 09:45:32 +0800

    初始化项目

---
+++ src/components/home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        this is home div
+    </div>
+</template>
+<script>
+    export default {
+        name:"home",
+        data() {
+            return {
+
+            }
+        },
+        mouted() {
+
+        },
+        methods: {
+            
+        }
+    }
+</script>
